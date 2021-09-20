7c62abb728d158e9218c5709610d39128f681021
FahimWeblogicAndCyberTechnologies <weblogicfahim@gmail.com>
2021-09-20 14:19:46 -0400

    HtoMPHconverter

---
+++ main.py
@@ -0,0 +1,12 @@
+# Program to convert kilometers into miles
+# Input is provided by the user in kilometers
+
+# take input from the user
+kilometers = float(input('How many kilometers?: '))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
